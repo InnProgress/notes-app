fd5ecfd7dff127838db50c1a8d543c4f55eafa55
InnProgress <arnoldautuch4@gmail.com>
2019-12-14 11:32:30 +0200

    Updates

---
+++ precache-manifest.dd06df69baf6e43cb9957c83beda40dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f45868dac76816f26fd3d33227655b",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "e7473aabc051ac04935f",
+    "url": "/notes-app/static/css/2.c205e385.chunk.css"
+  },
+  {
+    "revision": "3edc6713c5cda73d5136",
+    "url": "/notes-app/static/css/main.349b0c09.chunk.css"
+  },
+  {
+    "revision": "e7473aabc051ac04935f",
+    "url": "/notes-app/static/js/2.4a22d218.chunk.js"
+  },
+  {
+    "revision": "218bf88f19342317257889081e0c0a53",
+    "url": "/notes-app/static/js/2.4a22d218.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3edc6713c5cda73d5136",
+    "url": "/notes-app/static/js/main.bd67d442.chunk.js"
+  },
+  {
+    "revision": "3c7cb077c048240f0124",
+    "url": "/notes-app/static/js/runtime-main.f3b81749.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/notes-app/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/notes-app/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/notes-app/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/notes-app/static/media/notification.c0d3c94c.eot"
+  }
+]);
